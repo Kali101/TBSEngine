b8d68bd56d96473f019b7ba3dcfa132be3ddc021
Kali101 <darnisock@gmail.com>
2013-06-20 16:04:24 -0700

    Setting up map script (git test)

--- TBSEngineProject/Assets/Scripts/MapEditor/Map.cs
+++ TBSEngineProject/Assets/Scripts/MapEditor/Map.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using GoodStuff.NaturalLanguage;
+using System.Linq;
 
 public class Map : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
